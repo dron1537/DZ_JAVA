4e9534a7380bb8aa8edc951594438176e5c017e7
dron1537 <64251349+dron1537@users.noreply.github.com>
2020-04-30 13:09:32 +0300

    Update and rename задача_1.2/Main.java to  задача_1.2/Main.java

--- задача_1.2/Main.java
+++ задача_1.2/Main.java
@@ -1,1 +1,53 @@
-WWW...
+public class Main {
+    public static void main (String[]args)
+    {System.out.println(2+3);
+        int number=5;
+    int number1=2;
+        System.out.println(number*number1);
+
+        double x=7.5;double a=0.0;double b=0.0;
+        double k=0.0; double z=0.0;
+        double lpr=0.0;
+        System.out.println(a=x*2);System.out.println(b=x*3);
+
+        k=x+1; System.out.println(k+=5);
+        System.out.println(z+=5);
+        System.out.println(k+=5);
+        k=x/2;
+
+
+        int f=4;
+        int h=7;
+        int c=0;
+        System.out.println("Способ 1  "+(c=f*10+h));
+        String an="4";
+        String an1="7";
+        System.out.println("Способ 2  "+"4"+"7");
+        System.out.println("Способ 3  "+"47");
+
+        int p=5;
+        p+=6;
+
+        System.out.println(p);
+
+        int v=125;
+        int v_3 = 0;
+        double v_33=0.0;
+
+        System.out.println(v_3=v/3);
+        System.out.println(v_33=x*1.0/3.0);
+
+
+        double d=0.0; double mod= d=x/7;
+        double e=0.0; e=x/7;
+        System.out.println(d);
+        System.out.println(x/4); System.out.println(e);
+
+
+            int s=7; int w=6;
+        if (s>w) System.out.println("s больше");
+        else if (s<w)System.out.println("w больше");
+
+
+    }
+}
